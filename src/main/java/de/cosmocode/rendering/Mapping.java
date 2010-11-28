5aea99ccce0efe3e630e6e2f46b5f3c5a8127d04
Willi Schönborn <willi.schoenborn@gmx.net>
2010-11-28 19:37:31 +0100

    added restriction to Mapping it's now illegal to remap certain types all theses types are collected in a static set in the Mapping interface

--- src/main/java/de/cosmocode/rendering/Mapping.java
+++ src/main/java/de/cosmocode/rendering/Mapping.java
@@ -16,17 +16,85 @@
 
 package de.cosmocode.rendering;
 
+import java.util.Iterator;
 import java.util.Map;
+
+import com.google.common.collect.ImmutableSet;
 
 /**
  * A mapping of {@link Class} to {@link ValueRenderer}
  * which can be used with {@link Renderer}s to plug-in
  * new functionality.
- *
+ * 
+ * <p>
+ *   <strong>Note</strong>, the following classes are not allowed
+ *   to be remapped:
+ *   
+ *  <ul>
+ *    <li>{@code boolean}</li>
+ *    <li>{@link Boolean}</li>
+ *    <li>{@code byte}</li>
+ *    <li>{@link Byte}</li>
+ *    <li>{@code short}</li>
+ *    <li>{@link Short}</li>
+ *    <li>{@code char}</li>
+ *    <li>{@link Character}</li>
+ *    <li>{@code int}</li>
+ *    <li>{@link Integer}</li>
+ *    <li>{@code long}</li>
+ *    <li>{@link Long}</li>
+ *    <li>{@code float}</li>
+ *    <li>{@link Float}</li>
+ *    <li>{@code double}</li>
+ *    <li>{@link Double}</li>
+ *    <li>{@link CharSequence}</li>
+ *    <li>{@code Object[]}</li>
+ *    <li>{@link Iterable}</li>
+ *    <li>{@link Iterator}</li>
+ *    <li>{@link Map}</li>
+ *  </ul>
+ * </p>
+ * 
  * @since 1.1
  * @author Willi Schoenborn
  */
 public interface Mapping extends Map<Class<?>, ValueRenderer<?>> {
+    
+    /**
+     * An immutable set, containing all classes which are not allowed to
+     * be remapped.
+     */
+    ImmutableSet<Class<?>> ILLEGAL = ImmutableSet.<Class<?>>builder().
+        
+            // primitives / low levels
+            add(boolean.class).
+            add(Boolean.class).
+            add(byte.class).
+            add(Byte.class).
+            add(short.class).
+            add(Short.class).
+            add(char.class).
+            add(Character.class).
+            add(int.class).
+            add(Integer.class).
+            add(long.class).
+            add(Long.class).
+            add(float.class).
+            add(Float.class).
+            add(double.class).
+            add(Double.class).
+            add(CharSequence.class).
+            
+            // compound
+            add(Object[].class).
+            add(Iterable.class).
+            add(Iterator.class).
+            add(Map.class).
+            
+            // special
+            add(Renderable.class).
+            
+            build();
 
     /**
      * Finds a {@link ValueRenderer} capable of rendering the given type.
